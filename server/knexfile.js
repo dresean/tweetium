08a37b88f753941c2a803752c0321b4aeee377d4
No Hints <mrrobot@Nos-MacBook-Pro.local>
2019-02-12 15:50:47 -0500

    debug

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,25 +1,40 @@
 // Update with your config settings.
 
 const pg = require('pg')
+require('dotenv').config({path: '../.env'})
 module.exports = {
 
   development: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
-    migrations: './migrations',
-    seeds: './seeds/dev',
+    // connection: process.env.DATABASE_URL,
+    connection: {
+      host: '127.0.0.1',
+      database: process.env.DATABASE_NAME,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_ACCESS_KEY,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './migrations'
+  },
+    seeds: {
+      directory: './seeds/dev'
+    },
     useNullAsDefault: true
   },
 
   production: {
-    client: 'postgresql',
-    connection: process.env.DATABASE_URL,
+    client: 'pg',
+    connection: {
+      database: process.env.DATABASE_NAME,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_ACCESS_KEY,
+    },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations',
       directory: './migrations'
     },
     seeds: {
