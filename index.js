63013d9109b4fa4d60c207dae13ad832ab147ad0
No Hints <mrrobot@Nos-MacBook-Pro.local>
2019-02-12 20:46:29 -0500

    setup static ui build

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ require('dotenv').config()
 
 const Router = express.Router()
 const server = express()
+const path = require('path')
 
 server.use(cors())
 server.use(express.json())
+server.use(express.static(path.join(__dirname, 'ui/build')));
 server.use(morgan('dev'))
 server.use(compression())
 
@@ -17,6 +19,13 @@ server.get('/', (req, res) => {
     res.status(200).json({Message: 'server up and running!'})
 })
 
+server.get('/api', (req, res) => {
+    res.status(200).json({Message: 'paths working fine!'})
+})
+
+server.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/ui/build/index.html'));
+});
 server.listen(port, () => {
     console.log(`server listening to ${port} \n\n\n`)
 })
