771b80c606721ab4ae938cec339ae3f001c639fa
No Hints <mrrobot@Nos-MacBook-Pro.local>
2019-02-12 19:52:12 -0500

    organize files

---
+++ knexfile.js
@@ -0,0 +1,41 @@
+// Update with your config settings.
+
+const pg = require('pg')
+require('dotenv').config({path: './.env'})
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: process.env.DEV_DATABASE_NAME,
+      user: process.env.DEV_DATABASE_USER,
+      password: process.env.DEV_PASSWORD,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './migrations'
+  },
+    seeds: {
+      directory: './seeds/dev/'
+    },
+    useNullAsDefault: true
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds/production'
+    },
+    ssl: true,
+    useNullAsDefault: true
+  }
+
+};
