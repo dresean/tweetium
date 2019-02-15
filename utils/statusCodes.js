42e6fbe266d22f8a71e2d88af4d18519ac02db3d
dresean <williamsdresean@gmail.com>
2019-02-14 20:12:46 -0500

    make utils and start route

---
+++ utils/statusCodes.js
@@ -0,0 +1,28 @@
+
+module.exports = {
+    success: {
+        ok: 200,
+        created: 201,
+        accepted: 202,
+        noContent: 204,
+        resetContent: 205
+    },
+    redirection: {
+        movedPermanently: 301,
+        found: 302,
+        temporarilyRedirect: 307,
+        permanentRedirect: 308
+    },
+    clientError: {
+        badRequest: 400,
+        unauthorized: 401,
+        forbidden: 403,
+        notFound: 404,
+        requestTimeout: 408
+    },
+    serverError: {
+        internalServerError: 500,
+        badGateway: 502,
+        serviceUnavailable: 503
+    }
+}
