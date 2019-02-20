3ab66985ca569c5632d8c488e8a35ab9a9de46ae
dresean <williamsdresean@gmail.com>
2019-02-19 21:20:55 -0500

    add untracked files

--- knexfile.js
+++ knexfile.js
@@ -1,26 +1,26 @@
 // Update with your config settings.
 
 const pg = require('pg')
+pg.defaults.ssl = true
 require('dotenv').config({path: './.env'})
 module.exports = {
-
-  development: {
+  development : {
     client: 'pg',
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds/production'
+    },
+    useNullAsDefault: true,
     connection: {
       database: process.env.DEV_DATABASE_NAME,
       user: process.env.DEV_DATABASE_USER,
       password: process.env.DEV_PASSWORD,
     },
-    migrations: {
-      tableName: 'knex_migrations',
-      directory: './migrations'
+    ssl: true
   },
-    seeds: {
-      directory: './seeds/dev/'
-    },
-    useNullAsDefault: true
-  },
-
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
@@ -34,8 +34,7 @@ module.exports = {
     seeds: {
       directory: './seeds/production'
     },
-    ssl: true,
-    useNullAsDefault: true
+    useNullAsDefault: true,
+    ssl: true
   }
-
 };
