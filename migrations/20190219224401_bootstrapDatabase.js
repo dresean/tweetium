f9822420ef211bc9ee2235759fb1adabec26b4cd
dresean <williamsdresean@gmail.com>
2019-02-19 22:46:52 -0500

    add text column to Message table

--- migrations/20190219224401_bootstrapDatabase.js
+++ migrations/20190219224401_bootstrapDatabase.js
@@ -55,6 +55,7 @@ exports.up = function(knex, Promise) {
                 tbl.increments('messageId').unsigned().notNullable().primary()
                 tbl.integer('sent_by_id').references('User.userId').notNullable().onDelete('CASCADE').onUpdate('CASCADE')
                 tbl.integer('recipient_id').references('User.userId').notNullable().onDelete('CASCADE').onUpdate('CASCADE')
+                tbl.text('body').notNullable().defaultTo('Lets Chat!')
                 tbl.binary('image1')
                 tbl.binary('image2')
                 tbl.binary('image3')
@@ -85,4 +86,4 @@ exports.down = function(knex, Promise) {
         .catch(err => {
             console.log('All tables were not dropped successfully! \n', err)
         })
-}
+    }
