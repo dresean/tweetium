ed425d1f839d22f1635a463cdbfa209acc5c2210
dresean <williamsdresean@gmail.com>
2019-02-21 19:02:13 -0500

    make seeds util

--- utils/makeSeeds.js
+++ utils/makeSeeds.js
@@ -1,52 +1,75 @@
-// const faker = (number) => require('faker');
+const faker = (number) => require('faker');
 
 
-// const fakeUser = (number) => {
-//     return {
-//         userId: number,
-//         name: faker.name.firstName(),
-//         username: faker.internet.username(),
-//         email: faker.internet.email(),
-//         bio: faker.lorem.sentence(),
-//         tagline: faker.lorem.words(),
-//         private: faker.random.boolean(),
-//         emailVerified: faker.random.boolean()
-//     }
-// }
+const fakeUser = (number) => {
+    return {
+        userId: number,
+        name: faker.name.firstName(),
+        username: faker.internet.username(),
+        email: faker.internet.email(),
+        bio: faker.lorem.sentence(),
+        tagline: faker.lorem.words(),
+        private: faker.random.boolean(),
+        emailVerified: faker.random.boolean()
+    }
+}
 
-// const fakeTweet = (number) => {
-//     return {
-//         tweetId: number,
-//         user_id: number,
-//         body: faker.lorem.paragraph(),
-//     }
-// }
+const fakeTweet = (number) => {
+    return {
+        tweetId: number,
+        user_id: number,
+        body: faker.lorem.paragraph(),
+    }
+}
 
-// const fakeLike = (number) => {
-//     return {
-//     likeId: number,
-//     user_id: number,
-//     tweetId: number,
-//     }
-// }
+const fakeLike = (number) => {
+    return {
+    likeId: number,
+    user_id: number,
+    tweet_id: number,
+    }
+}
 
-// const fakeReply = (number) => {
-//     return {
-//         replyId: number,
-//         user_id: number,
-//         tweet_id: number
-//     }
-// }
+const fakeReply = (number) => {
+    return {
+        replyId: number,
+        user_id: number,
+        tweet_id: number,
+        body: faker.lorem.paragraph()
+    }
+}
 
-// const fakeRetweet = (number) => {
+const fakeRetweet = (number) => {
+    return {
+        retweetId: number,
+        user_id: number,
+        tweet_id: number,
+        body: faker.lorem.paragraph()
+    }
+}
 
-// }
+const fakeFollow = (number) => {
+    return {
+        followed_id: number,
+        follower_id: number + 1 
+    }
+}
 
-// const fakeFollower = (number) => {
+const fakeMessage = (number) => {
+    return {
+        messageId: number,
+        to_id: number,
+        from_id: number + 1,
+        body: faker.lorem.paragraph()
+    }
+}
 
-// }
-
-// const fakeMessage = (number) => {
-
-// }
-
+module.exports = {
+    fakeUser,
+    fakeTweet,
+    fakeRetweet,
+    fakeFollow,
+    fakeMessage,
+    fakeReply,
+    fakeLike
+}
