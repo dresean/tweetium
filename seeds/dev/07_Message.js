5cba4a204ee13204f732f0e6b45d5ca50856f8d5
dresean <williamsdresean@gmail.com>
2019-02-23 21:53:18 -0500

    seed entire database

--- seeds/dev/07_Message.js
+++ seeds/dev/07_Message.js
@@ -1,11 +1,10 @@
-const { fakeFollow, multiply } = require('../../utils/makeSeeds')
-
+const { fakeMessage, multiply } = require('../../utils/makeSeeds')
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('Follow').del()
+  return knex('Message').del()
     .then(function () {
       // Inserts seed entries
-      return knex('Follow').insert(multiply(fakeFollow, 34));
+      return knex('Message').insert(multiply(fakeMessage, 34));
     })
     .catch(err => console.log(err.message))
 };
