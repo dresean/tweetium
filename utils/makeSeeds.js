3c50189c8e92762674e675e943ee52829a75d6c1
dresean <williamsdresean@gmail.com>
2019-02-23 22:21:35 -0500

    makeSeeds.js complete

--- utils/makeSeeds.js
+++ utils/makeSeeds.js
@@ -1,52 +1,126 @@
-// const faker = (number) => require('faker');
+const faker = require('faker');
 
+// limits string lenght output from faker
 
-// const fakeUser = (number) => {
-//     return {
-//         userId: number,
-//         name: faker.name.firstName(),
-//         username: faker.internet.username(),
-//         email: faker.internet.email(),
-//         bio: faker.lorem.sentence(),
-//         tagline: faker.lorem.words(),
-//         private: faker.random.boolean(),
-//         emailVerified: faker.random.boolean()
-//     }
-// }
+const makeString = () => {
+const maxLength = 280
+let str = faker.lorem.paragraph()
+    if (str.length > maxLength) {
+        str = str.slice(0, maxLength - 1)
+    }
+    return str
+}
 
-// const fakeTweet = (number) => {
-//     return {
-//         tweetId: number,
-//         user_id: number,
-//         body: faker.lorem.paragraph(),
-//     }
-// }
+    // generate fake data, each function simply returns an object 
 
-// const fakeLike = (number) => {
-//     return {
-//     likeId: number,
-//     user_id: number,
-//     tweetId: number,
-//     }
-// }
+const fakeUser = (number) => {
+    return {
+        userId: number,
+        name: faker.name.firstName(),
+        username: faker.internet.userName(),
+        email: faker.internet.email(),
+        bio: faker.lorem.sentence(),
+        tagline: faker.lorem.words(),
+        private: faker.random.boolean(),
+        emailVerified: faker.random.boolean()
+    }
+}
 
-// const fakeReply = (number) => {
-//     return {
-//         replyId: number,
-//         user_id: number,
-//         tweet_id: number
-//     }
-// }
+const fakeTweet = (number) => {
+    return {
+        tweetId: number,
+        user_id: number,
+        body: makeString()
+    }
+}
 
-// const fakeRetweet = (number) => {
+const fakeLike = (number) => {
+    return {
+    likeId: number,
+    user_id: number,
+    tweet_id: number,
+    }
+}
 
-// }
+const fakeReply = (number) => {
+    return {
+        replyId: number,
+        user_id: number,
+        tweet_id: number,
+        body: makeString()
+    }
+}
 
-// const fakeFollower = (number) => {
+const fakeReplyRetweet = (number) => {
+    return {
+        retweetId: number,
+        user_id: number,
+        reply_id: number,
+        body: makeString()
+    }
+}
 
-// }
+const fakeRetweet = (number) => {
+    return {
+        retweetId: number,
+        user_id: number,
+        tweet_id: number,
+        body: makeString()
+    }
+}
 
-// const fakeMessage = (number) => {
+const fakeFollow = (number) => {
+    return {
+        followed_id: number,
+        follower_id: number + 1 
+    }
+}
 
-// }
+const fakeMessage = (number) => {
+    return {
+        messageId: number,
+        to_id: number,
+        from_id: number + 1,
+        body: faker.lorem.paragraph()
+    }
+}
 
+// allows optional arguements
+// one for loop length
+// two for loop start and end
+
+const multiply = (cb, ...rest) => {
+    let i
+    let condition
+    const arr = []
+    switch(rest.length) {
+        case 2:
+        i = rest[0]
+        condition = rest[0] + rest[1]
+        break;
+        case 1:
+        i = 0
+        condition = rest[0]
+        break;
+        default:
+        i = 0
+        condition = 35
+        break;
+    }
+    for(i; i <= condition; i++) {
+        arr.push(cb(i))
+    }
+    return arr
+}
+
+module.exports = {
+    fakeUser,
+    fakeTweet,
+    fakeLike,
+    fakeReply,
+    fakeRetweet,
+    fakeFollow,
+    fakeMessage,
+    multiply,
+    fakeReplyRetweet,
+}
