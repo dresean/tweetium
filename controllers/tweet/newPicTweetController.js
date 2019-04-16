db0bf2fcb71d275c766b372146797b10039ca35e
dresean <williamsdresean@gmail.com>
2019-04-16 01:04:08 -0400

    start tweet with media database query

--- controllers/tweet/newPicTweetController.js
+++ controllers/tweet/newPicTweetController.js
@@ -3,8 +3,24 @@ const db = require('../../db')
 const incrementTweetCount = (userId) => {
     let query = db('User')
     return query
-    .where('userId', userId)
     .increment('tweets', 1)
+    .where('userId', userId)
 }
 
-const postTweetWithImage = (content, userId, image)
+const postTweetWithImage = (content, userId, imageUrl, req) => {
+    let query = db('Tweet')
+    return query
+    .insert([
+        {'user_id': userId}, 
+        {'image': imageUrl}, 
+        {'content': req.body.content}
+        ], 
+        ['tweetId', 'content', 'image']
+    )
+}
+
+
+module.exports = { incrementTweetCount, postTweetWithImage }
+
+
+// TODO set up jwt with user avatar available in req object
--- routes/tweet/newPicTweetRoute.js
+++ routes/tweet/newPicTweetRoute.js
@@ -2,27 +2,38 @@ const express = require('express')
 const Router = express.Router()
 const { upload } = require('../../services/awsUpload')
 const { success, clientError, serverError } = require('../../utils/statusCodes')
-
+const { postTweetWithImage, incrementTweetCount } = require('../../controllers/tweet/newPicTweetController')
 
 const singleImage = upload.single('image')
-const multipleImages = upload.array('image', 3)
 
 Router.post('/user/:username/tweet-upload', (req, res) => {
     const username = req.params.username
     const userId = req.userId
     const currentUsername = req.username
     const { content } = req.body
+    let imageUrl
 
     if(!userId || username !== currentUsername) {
         return res.status(clientError.unauthorized).json({Message: 'You must be logged in to do that.'})
     }
-
-    return singleImage(req, res, (err, pic) => {
+    singleImage(req, res, (err, pic) => {
         if(err) {
             console.log('There was an error uploading the image \n\n\n', err)
             return res.status(serverError.internalServerError)
         }
-        return res.status(success.created).json({'imageUrl': req.file.location, content})
+        return incrementTweetCount(userId)
+        .then(() => {
+        return postTweetWithImage(content, userId, req.file.location, req)
+        })
+        .then(response => {
+        console.log('Tweet successfully posted!')
+        return res.status(success.created).json({Message: 'Tweet successfully posted!', response})
+    })
+    .catch(err => {
+        console.log('There was an error. \n\n\n', err)
+        return res.status(serverError.internalServerError).json({Message: 'There was a problem posting your tweet, please try again later.'})
+    })
+        // return res.status(success.created).json({'imageUrl': req.file.location, response})
     })
 })
 
