1b23132edf7803b731ac7a0fb39599ba1703b51c
dresean <williamsdresean@gmail.com>
2019-04-16 14:31:23 -0400

    add new tweet with pic upload feature

--- routes/tweet/newPicTweetRoute.js
+++ routes/tweet/newPicTweetRoute.js
@@ -2,7 +2,7 @@ const express = require('express')
 const Router = express.Router()
 const { upload } = require('../../services/awsUpload')
 const { success, clientError, serverError } = require('../../utils/statusCodes')
-
+const { incrementTweetCount, postTweetWithImage } = require('../../controllers/tweet/newPicTweetController')
 
 const singleImage = upload.single('image')
 const multipleImages = upload.array('image', 3)
@@ -12,6 +12,7 @@ Router.post('/user/:username/tweet-upload', (req, res) => {
     const userId = req.userId
     const currentUsername = req.username
     const { content } = req.body
+    let theImageFile
 
     if(!userId || username !== currentUsername) {
         return res.status(clientError.unauthorized).json({Message: 'You must be logged in to do that.'})
@@ -22,7 +23,22 @@ Router.post('/user/:username/tweet-upload', (req, res) => {
             console.log('There was an error uploading the image \n\n\n', err)
             return res.status(serverError.internalServerError)
         }
-        return res.status(success.created).json({'imageUrl': req.file.location, content})
+        theImageFile = req.file
+        console.log(theImageFile)
+
+        return incrementTweetCount(userId)
+            .then(() => {
+            return postTweetWithImage(content, userId, theImageFile, req)
+            })
+            .then(response => {
+            console.log('Tweet successfully posted!')
+            return res.status(success.created).json({Message: 'Tweet successfully posted!', response})
+        })
+        .catch(err => {
+        console.log('There was an error. \n\n\n', err)
+        return res.status(serverError.internalServerError).json({Message: 'There was a problem posting your tweet, please try again later.'})
+    })
+        // return res.status(success.created).json({'imageUrl': req.file.location, content})
     })
 })
 
@@ -31,17 +47,6 @@ module.exports = Router
 
 // TODO add code below and insert the upload as 'image' in the Tweet Table.
 
-// return incrementTweetCount(userId)
-//         .then(() => {
-//         return postTweetWithImage(content, userId, imageUrl, req)
-//         })
-//         .then(response => {
-//         console.log('Tweet successfully posted!')
-//         return res.status(success.created).json({Message: 'Tweet successfully posted!', response})
-//     })
-//     .catch(err => {
-//         console.log('There was an error. \n\n\n', err)
-//         return res.status(serverError.internalServerError).json({Message: 'There was a problem posting your tweet, please try again later.'})
-//     })
+
 module.exports = Router
 
