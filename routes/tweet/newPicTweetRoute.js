3436c60b5ae4bb09aefe81f1bf818542d33df512
dresean <williamsdresean@gmail.com>
2019-04-17 19:09:30 -0400

    clean up routes and controllers

--- routes/tweet/newPicTweetRoute.js
+++ routes/tweet/newPicTweetRoute.js
@@ -5,15 +5,15 @@ const { success, clientError, serverError } = require('../../utils/statusCodes')
 const { incrementTweetCount, postTweetWithImage } = require('../../controllers/tweet/newPicTweetController')
 
 const singleImage = upload.single('image')
-const multipleImages = upload.array('image', 3)
+let imageFile
 
 Router.post('/user/:username/tweet-upload', (req, res) => {
     const username = req.params.username
     const userId = req.userId
     const currentUsername = req.username
     const { content } = req.body
-    let theImageFile
-
+    
+    console.log('req \n', req)
     if(!userId || username !== currentUsername) {
         return res.status(clientError.unauthorized).json({Message: 'You must be logged in to do that.'})
     }
@@ -23,22 +23,18 @@ Router.post('/user/:username/tweet-upload', (req, res) => {
             console.log('There was an error uploading the image \n\n\n', err)
             return res.status(serverError.internalServerError)
         }
-        theImageFile = req.file
-        console.log(theImageFile)
-
         return incrementTweetCount(userId)
             .then(() => {
-            return postTweetWithImage(content, userId, theImageFile.location, req)
+                return postTweetWithImage(content, userId, imageFile, req)
             })
             .then(response => {
-            console.log('Tweet successfully posted!')
-            return res.status(success.created).json({Message: 'Tweet successfully posted!', response})
-        })
-        .catch(err => {
-        console.log('There was an error. \n\n\n', err)
-        return res.status(serverError.internalServerError).json({Message: 'There was a problem posting your tweet, please try again later.'})
-    })
-        // return res.status(success.created).json({'imageUrl': req.file.location, content})
+                console.log('Tweet successfully posted!')
+                return res.status(success.created).json({Message: 'Tweet successfully posted!', response})
+            })
+            .catch(err => {
+                console.log('There was an error. \n\n\n', err)
+                return res.status(serverError.internalServerError).json({Message: 'There was a problem posting your tweet, please try again later.'})
+            })
     })
 })
 
